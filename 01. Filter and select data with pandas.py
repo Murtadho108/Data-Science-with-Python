1c35c22bb5fc54a381bc106810c818e949838281
Murtadho108 <33507864+Murtadho108@users.noreply.github.com>
2019-02-01 15:52:16 +0700

    Update 01. Filter and select data with pandas.py

--- 01. Filter and select data with pandas.py
+++ 01. Filter and select data with pandas.py
@@ -1,12 +1,6 @@
-
-# coding: utf-8
-
 # # Basics of Data Munging
 
 # ## Filtering and selecting data
-
-# In[6]:
-
 
 import numpy as np
 import pandas as pd
@@ -15,26 +9,17 @@ from pandas import Series, DataFrame
 
 # ## Selecting and retrieving data
 
-# In[ ]:
 
 
 series_obj = Series(np.arange(8), index = ['row 1', 'row 2', 'row 3', 'row 4', 'row 5', 'row 6', 'row 7', 'row 8'])
 series_obj
 
 
-# In[10]:
-
-
 series_obj['row 7']
-
-
-# In[11]:
 
 
 series_obj[[0,7]]
 
-
-# In[26]:
 
 
 np.random.seed(25)
@@ -42,8 +27,6 @@ DF_obj = DataFrame (np.random.rand(36).reshape((6,6)), index = ['row 1', 'row 2'
                    columns = ['column 1', 'column 2', 'column 3', 'column 4', 'column 5', 'column 6'])
 DF_obj
 
-
-# In[27]:
 
 
 DF_obj.loc[['row 2', 'row 4'],['column 1','column 5']]
@@ -55,7 +38,6 @@ DF_obj.loc[['row 2', 'row 4'],['column 1','column 5']]
 
 # ## Comparing with scalars
 
-# In[32]:
 
 
 DF_obj < 0.2
@@ -63,7 +45,6 @@ DF_obj < 0.2
 
 # ## Filtering with scalars 
 
-# In[37]:
 
 
 series_obj[series_obj > 3]
@@ -71,7 +52,7 @@ series_obj[series_obj > 3]
 
 # ## Setting values with scalars
 
-# In[42]:
+
 
 
 series_obj['row 1','row 5', 'row 7']= 8
